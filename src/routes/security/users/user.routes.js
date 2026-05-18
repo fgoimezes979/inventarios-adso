5e03e4a31e269a3317b149ef56d0c4a29d6e4ec6
Fabio Gomez <fabiogomez@MacBook-Pro-de-Fabio.local>
2026-05-18 13:33:32 -0500

    fix user routes

--- src/routes/security/users/user.routes.js
+++ src/routes/security/users/user.routes.js
@@ -1,35 +1,9 @@
 const { Router } = require("express");
 const router = Router();
 
-/** importar middleware */
-const authMiddleware = require("../../../middlewares/auth.middleware");
+const userController = require("../../../controllers/parameters/security/user.controller");
 
-/** importar controladores */
-const {
-  login,
-  index,
-  create,
-  show,
-  update,
-  destroy
-} = require("../../../controllers/parameters/security/user.controller");
+// LOGIN
+router.post("/login", userController.login);
 
-/** ============================
- *  RUTA PUBLICA
- ============================ */
-router.post("/login", login);
-
-/** ============================
- *  PROTEGER TODO LO DEMÁS
- ============================ */
-router.use(authMiddleware);
-
-/** rutas protegidas */
-router.get("/", index);
-router.post("/", create);
-router.get("/:id", show);
-router.put("/:id", update);
-router.delete("/:id", destroy);
-
-/** exportar modulo */
 module.exports = router;
